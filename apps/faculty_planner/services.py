2653839ec84a1a03b9eb86933337d63d415b6e0e
chris.lego <poputeacristi@gmail.com>
2018-04-04 23:57:06 +0300

    read data from pdf

--- apps/faculty_planner/services.py
+++ apps/faculty_planner/services.py
@@ -1,3 +1,8 @@
+# from wand.image import Image as Img
+import os
+from PIL import Image
+from pytesseract import image_to_string
+
 from .models import Student, StudentSpecialization, Specialization
 
 
@@ -11,6 +16,15 @@ def store_specialization(student_uuid, specialization_uuid):
 
     return student_specialization
 
+
 # TODO: define service that will be used in the import part to save date
 
+# with Img(filename='file_name.pdf', resolution=300) as img:
+#     img.compression_quality = 99
+#     img.save(filename='image_name.jpg')
+script_dir = os.path.dirname(__file__)
+print(script_dir)
+img_data = Image.open('/Users/cristian.poputea/projects/faculty-planner-django/apps/faculty_planner/test.png')
 
+text = image_to_string(img_data)
+print(text)
